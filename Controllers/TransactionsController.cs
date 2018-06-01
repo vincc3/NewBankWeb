b35b9345e89f7161b022bbadff9f17b433022b7e
vincc3 <38580185+vincc3@users.noreply.github.com>
2018-06-01 21:57:17 +1000

    Initial commit

---
+++ Controllers/TransactionsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using NewBankWeb.Models;
+
+namespace NewBankWeb.Controllers
+{
+    public class TransactionsController : ApiController
+    {
+        private MyDatabaseEntities db = new MyDatabaseEntities();
+
+        // GET: api/Transactions
+        public IQueryable<Transaction> GetTransactions()
+        {
+            return db.Transactions;
+        }
+
+        // GET: api/Transactions/5
+        [ResponseType(typeof(Transaction))]
+        public IHttpActionResult GetTransaction(int id)
+        {
+            Transaction transaction = db.Transactions.Find(id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(transaction);
+        }
+
+        // PUT: api/Transactions/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTransaction(int id, Transaction transaction)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != transaction.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(transaction).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TransactionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Transactions
+        [ResponseType(typeof(Transaction))]
+        public IHttpActionResult PostTransaction(Transaction transaction)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Transactions.Add(transaction);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = transaction.Id }, transaction);
+        }
+
+        // DELETE: api/Transactions/5
+        [ResponseType(typeof(Transaction))]
+        public IHttpActionResult DeleteTransaction(int id)
+        {
+            Transaction transaction = db.Transactions.Find(id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+
+            db.Transactions.Remove(transaction);
+            db.SaveChanges();
+
+            return Ok(transaction);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TransactionExists(int id)
+        {
+            return db.Transactions.Count(e => e.Id == id) > 0;
+        }
+    }
+}
